c44fad0fcaf15b299b98513bbda4348ce996b39d
samsullivandelgobbo <sam.sullivan150@gmail.com>
2023-11-19 23:53:47 -0500

    UI clean up, shopify headless integration, shadcn svelte testing (works great), admin dashboard coming together

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -8,11 +8,9 @@ export async function GET() {
 		{ url: '/adopt', changefreq: 'daily', priority: 1 },
 		{ url: '/shop', changefreq: 'daily', priority: 1 },
 		{ url: '/donate', changefreq: 'daily', priority: 1 },
-		{ url: '/blog', changefreq: 'daily', priority: 1 }
-    { url: '/adopt/application', changefreq: 'daily', priority: 1 },
-    { url: '/care', changefreq: 'daily', priority: 1 }
-
-
+		{ url: '/blog', changefreq: 'daily', priority: 1 },
+		{ url: '/adopt/application', changefreq: 'daily', priority: 1 },
+		{ url: '/care', changefreq: 'daily', priority: 1 }
 	];
 
 	const stream = new SitemapStream({ hostname: 'https://canadianchinchilla.ca' });
